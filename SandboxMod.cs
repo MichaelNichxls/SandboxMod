ca5da5107def4369b4d8308df13aae64fe79079b
Michael Nichols <mjnichols10@yahoo.com>
2022-03-03 17:39:13 -0600

    Added ModRecipe extensions, along with several bidirectional recipes.

--- SandboxMod.cs
+++ SandboxMod.cs
@@ -1,3 +1,5 @@
+using SandboxMod.Common.Extensions;
+using SandboxMod.Content.Tiles;
 using Terraria;
 using Terraria.ID;
 using Terraria.Localization;
@@ -7,6 +9,155 @@ namespace SandboxMod
 {
     public partial class SandboxMod : Mod
     {
+        public override void AddRecipes()
+        {
+            #region Pre-Hardmode Ores
+            // Make helper
+            var recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.CopperOre);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.TinOre);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.CopperBar);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.TinBar);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.IronOre);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.LeadOre);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.IronBar);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.LeadBar);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.SilverOre);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.TungstenOre);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.SilverBar);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.TungstenBar);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.GoldOre);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.PlatinumOre);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.GoldBar);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.PlatinumBar);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.DemoniteOre);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.CrimtaneOre);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.DemoniteBar);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.CrimtaneBar);
+            recipe.AddBidirectionalRecipe();
+            #endregion Pre-Hardmode Ores
+            #region Hardmode Ores
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.CobaltOre);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.PalladiumOre);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.CobaltBar);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.PalladiumBar);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.MythrilOre);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.OrichalcumOre);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.MythrilBar);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.OrichalcumBar);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.AdamantiteOre);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.TitaniumOre);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.AdamantiteBar);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.TitaniumBar);
+            recipe.AddBidirectionalRecipe();
+            #endregion Hardmode Ores
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.ShadowScale);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.TissueSample);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.UnholyWater);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.BloodWater);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.CursedFlame);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.Ichor);
+            recipe.AddBidirectionalRecipe();
+
+            recipe = new ModRecipe(this);
+
+            recipe.AddIngredient(ItemID.FlaskofCursedFlames);
+            recipe.AddTile(ModContent.TileType<MaterialTransmuter>());
+            recipe.SetResult(ItemID.FlaskofIchor);
+            recipe.AddBidirectionalRecipe();
+
+            // etc.
+        }
+
         public override void AddRecipeGroups()
         {
             var group = new RecipeGroup(() => $"{Language.GetTextValue("LegacyMisc.37")} Evil Bar", new int[]
@@ -14,8 +165,8 @@ namespace SandboxMod
                 ItemID.DemoniteBar,
                 ItemID.CrimtaneBar
             });
-            RecipeGroup.RegisterGroup($"{nameof(SandboxMod)}:EvilBar", group);
 
+            RecipeGroup.RegisterGroup($"{nameof(SandboxMod)}:EvilBar", group);
             // etc.
         }
     }
