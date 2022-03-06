42343c6944d037e06db8648f3fa2f2899d43cd04
Michael Nichols <mjnichols10@yahoo.com>
2022-03-06 14:44:17 -0600

    Added a basic wall (WIP).

---
+++ Content/Items/Walls/BasicWall.cs
@@ -0,0 +1,27 @@
+﻿using SandboxMod.Common;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SandboxMod.Content.Items.Walls
+{
+    public class BasicWall : ModItem
+    {
+        public override string Texture => AssetDirectory.GetTexture<BasicWall>();
+
+        public override void SetStaticDefaults() => Tooltip.SetDefault("A basic wall");
+
+        public override void SetDefaults()
+        {
+            item.consumable     = true;
+            item.createWall     = ModContent.WallType<Content.Walls.BasicWall>();
+            item.width          = 16;
+            item.height         = 16;
+            item.maxStack       = 999;
+            item.useTime        = 7;
+            item.useAnimation   = 15;
+            item.useStyle       = ItemUseStyleID.SwingThrow;
+            item.useTurn        = true;
+            item.autoReuse      = true;
+        }
+    }
+}
---
+++ Content/Walls/BasicWall.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using SandboxMod.Common;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SandboxMod.Content.Walls
+{
+    public class BasicWall : ModWall
+    {
+        public override bool Autoload(ref string name, ref string texture)
+        {
+            texture = AssetDirectory.GetTexture<BasicWall>();
+            return base.Autoload(ref name, ref texture);
+        }
+
+        public override void SetDefaults()
+        {
+            // Needs more here
+            Main.wallHouse[Type] = true;
+
+            drop = ModContent.ItemType<Items.Walls.BasicWall>();
+            dustType = DustID.Sparkle; // Make custom dust
+
+            AddMapEntry(new Color(200, 200, 200));
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num) =>
+            num = fail ? 1 : 3;
+
+        //public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b) =>
+        //    (r, g, b) = (1f / byte.MaxValue, 152f / byte.MaxValue, 220f / byte.MaxValue);
+    }
+}
