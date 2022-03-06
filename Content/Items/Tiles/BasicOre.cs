0177e372b8f742d1e875238080d42044dc759b10
Michael Nichols <mjnichols10@yahoo.com>
2022-03-05 22:21:53 -0600

    Added a basic ore.

---
+++ Content/Items/Tiles/BasicOre.cs
@@ -0,0 +1,30 @@
+﻿using SandboxMod.Common;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SandboxMod.Content.Items.Tiles
+{
+    public class BasicOre : ModItem
+    {
+        public override string Texture => AssetDirectory.GetTexture<BasicOre>();
+
+        public override void SetStaticDefaults() => Tooltip.SetDefault("A basic ore");
+
+        public override void SetDefaults()
+        {
+            item.consumable     = true;
+            item.createTile     = ModContent.TileType<Content.Tiles.BasicOre>();
+            item.rare           = ItemRarityID.Green;
+            item.value          = Item.sellPrice(silver: 2, copper: 50);
+            item.width          = 16;
+            item.height         = 16;
+            item.maxStack       = 999;
+            item.useTime        = 10;
+            item.useAnimation   = 15;
+            item.useStyle       = ItemUseStyleID.SwingThrow;
+            item.useTurn        = true;
+            item.autoReuse      = true;
+        }
+    }
+}
---
+++ Content/Tiles/BasicOre.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using SandboxMod.Common;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SandboxMod.Content.Tiles
+{
+    public class BasicOre : ModTile
+    {
+        public override bool Autoload(ref string name, ref string texture)
+        {
+            texture = AssetDirectory.GetTexture<BasicOre>();
+            return base.Autoload(ref name, ref texture);
+        }
+
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type]        = true;
+            Main.tileMergeDirt[Type]    = true;
+            Main.tileBlockLight[Type]   = true;
+            Main.tileSpelunker[Type]    = true;
+            Main.tileValue[Type]        = 420;
+            Main.tileShine[Type]        = 975;
+            Main.tileShine2[Type]       = true;
+            Main.tileLighted[Type]      = true;
+
+            TileID.Sets.Ore[Type] = true;
+
+            minPick     = 65;
+            mineResist  = 2.5f;
+            drop        = ModContent.ItemType<Items.Tiles.BasicOre>();
+            dustType    = DustID.IceRod;
+            soundType   = SoundID.Tink;
+            soundStyle  = 1;
+
+            ModTranslation name = CreateMapEntryName();
+
+            name.SetDefault("Basic Ore");
+            AddMapEntry(new Color(12, 224, 255), name);
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num) =>
+            num = fail ? 1 : 3;
+
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b) =>
+            (r, g, b) = (1f / byte.MaxValue, 152f / byte.MaxValue, 220f / byte.MaxValue);
+    }
+}
