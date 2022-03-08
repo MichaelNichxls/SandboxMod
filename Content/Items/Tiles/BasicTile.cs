56f0db4cad51d5f5b2663a6a8322b71844c1d38f
Michael Nichols <mjnichols10@yahoo.com>
2022-03-07 18:21:18 -0600

    Added a basic tile and modified the basic wall's map entry color to be darker.

---
+++ Content/Items/Tiles/BasicTile.cs
@@ -0,0 +1,27 @@
+﻿using SandboxMod.Common;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SandboxMod.Content.Items.Tiles
+{
+    public class BasicTile : ModItem
+    {
+        public override string Texture => AssetDirectory.GetTexture<BasicTile>();
+
+        public override void SetStaticDefaults() => Tooltip.SetDefault("A basic tile");
+
+        public override void SetDefaults()
+        {
+            item.consumable     = true;
+            item.createTile     = ModContent.TileType<Content.Tiles.BasicTile>();
+            item.width          = 16;
+            item.height         = 16;
+            item.maxStack       = 999;
+            item.useTime        = 10;
+            item.useAnimation   = 15;
+            item.useStyle       = ItemUseStyleID.SwingThrow;
+            item.useTurn        = true;
+            item.autoReuse      = true;
+        }
+    }
+}
--- Content/Tiles/BasicTile.cs
+++ Content/Tiles/BasicTile.cs
@@ -4,24 +4,27 @@ using SandboxMod.Content.Dusts;
 using Terraria;
 using Terraria.ModLoader;
 
-namespace SandboxMod.Content.Walls
+namespace SandboxMod.Content.Tiles
 {
-    public class BasicWall : ModWall
+    public class BasicTile : ModTile
     {
         public override bool Autoload(ref string name, ref string texture)
         {
-            texture = AssetDirectory.GetTexture<BasicWall>();
+            texture = AssetDirectory.GetTexture<BasicTile>();
             return base.Autoload(ref name, ref texture);
         }
 
         public override void SetDefaults()
         {
-            Main.wallHouse[Type] = true;
+            Main.tileSolid[Type]        = true;
+            Main.tileMergeDirt[Type]    = true;
+            Main.tileBlockLight[Type]   = true;
+            Main.tileLighted[Type]      = true;
 
-            drop        = ModContent.ItemType<Items.Walls.BasicWall>();
+            drop        = ModContent.ItemType<Items.Tiles.BasicTile>();
             dustType    = ModContent.DustType<BasicDust>();
 
-            AddMapEntry(new Color(200, 200, 200));
+            AddMapEntry(new Color(255, 255, 255));
         }
 
         public override void NumDust(int i, int j, bool fail, ref int num) =>
