f87835acadd8c47267f88f3e15f6b7616c50e815
Michael Nichols <mjnichols10@yahoo.com>
2022-03-07 19:28:17 -0600

    Added a basic work bench and modified tiles' map entry colors some more.

---
+++ Content/Items/Tiles/BasicWorkBench.cs
@@ -0,0 +1,27 @@
+﻿using SandboxMod.Common;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SandboxMod.Content.Items.Tiles
+{
+    public class BasicWorkBench : ModItem
+    {
+        public override string Texture => AssetDirectory.GetTexture<BasicWorkBench>();
+
+        public override void SetStaticDefaults() => Tooltip.SetDefault("A basic work bench used for basic crafting");
+
+        public override void SetDefaults()
+        {
+            item.consumable     = true;
+            item.createTile     = ModContent.TileType<Content.Tiles.BasicWorkBench>();
+            item.width          = 32;
+            item.height         = 18;
+            item.maxStack       = 99;
+            item.useTime        = 10;
+            item.useAnimation   = 15;
+            item.useStyle       = ItemUseStyleID.SwingThrow;
+            item.useTurn        = true;
+            item.autoReuse      = true;
+        }
+    }
+}
---
+++ Content/Tiles/BasicWorkBench.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using SandboxMod.Common;
+using SandboxMod.Content.Dusts;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace SandboxMod.Content.Tiles
+{
+    public class BasicWorkBench : ModTile
+    {
+        public override bool Autoload(ref string name, ref string texture)
+        {
+            texture = AssetDirectory.GetTexture<BasicWorkBench>();
+            return base.Autoload(ref name, ref texture);
+        }
+
+        public override void SetDefaults()
+        {
+            // How to disable NPC step-up?
+            Main.tileFrameImportant[Type]   = true;
+            Main.tileSolidTop[Type]         = true;
+            Main.tileTable[Type]            = true;
+            Main.tileLavaDeath[Type]        = true;
+            Main.tileNoAttach[Type]         = true;
+            Main.tileLighted[Type]          = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
+            TileObjectData.newTile.CoordinateHeights = new int[] { 18 };
+            TileObjectData.addTile(Type);
+
+            AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
+
+            adjTiles            = new int[] { TileID.WorkBenches };
+            dustType            = ModContent.DustType<BasicDust>();
+            disableSmartCursor  = true;
+
+            ModTranslation name = CreateMapEntryName();
+
+            name.SetDefault("Basic Work Bench");
+            AddMapEntry(new Color(255, 255, 255), name);
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num) =>
+            num = fail ? 1 : 3;
+
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b) =>
+            (r, g, b) = (255f / byte.MaxValue, 255f / byte.MaxValue, 255f / byte.MaxValue);
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY) =>
+            Item.NewItem(new Vector2(i * 16, j * 16), new Vector2(32, 16), ModContent.ItemType<Items.Tiles.BasicWorkBench>());
+    }
+}
