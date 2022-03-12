018d0965887ec230c64416c35cef7a8f18d62392
Michael Nichols <mjnichols10@yahoo.com>
2022-03-11 23:55:05 -0600

    Added a basic torch (WIP).

---
+++ Content/Items/Tiles/BasicTorch.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using SandboxMod.Common;
+using SandboxMod.Content.Dusts;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SandboxMod.Content.Items.Tiles
+{
+    public class BasicTorch : ModItem
+    {
+        public override string Texture => AssetDirectory.GetTexture<BasicTorch>();
+
+        public override void SetStaticDefaults() => Tooltip.SetDefault("A basic torch that provides light");
+
+        public override void SetDefaults()
+        {
+            item.consumable     = true;
+            item.flame          = true;
+            item.noWet          = true;
+            item.createTile     = ModContent.TileType<Content.Tiles.BasicTorch>();
+            item.value          = Item.sellPrice(copper: 10);
+            item.Size           = new Vector2(14, 16);
+            item.maxStack       = 99;
+            item.useTime        = 10;
+            item.useAnimation   = 15;
+            item.useStyle       = ItemUseStyleID.SwingThrow;
+            item.holdStyle      = ItemHoldStyleID.HoldingOut;
+            item.useTurn        = true;
+            item.autoReuse      = true;
+        }
+
+        // Make formatting consistent
+        public override void HoldItem(Player player)
+        {
+            if (player.wet)
+                return;
+
+            if (Main.rand.Next(player.itemAnimation > 0 ? 40 : 80) == 0)
+                Dust.NewDust(new Vector2(player.itemLocation.X + (16f * player.direction), player.itemLocation.Y - (14f * player.gravDir)), 4, 4, ModContent.DustType<BasicDust>());
+
+            Lighting.AddLight(
+                player.RotatedRelativePoint(new Vector2(player.itemLocation.X + (12f * player.direction) + player.velocity.X, player.itemLocation.Y - 14f + player.velocity.Y)),
+                new Vector3(1f, 1f, 1f));
+        }
+
+        public override void AutoLightSelect(ref bool dryTorch, ref bool wetTorch, ref bool glowstick) =>
+            dryTorch = true;
+
+        public override void PostUpdate()
+        {
+            if (!item.wet)
+                Lighting.AddLight(item.Center, new Vector3(1f, 1f, 1f));
+        }
+    }
+}
---
+++ Content/Tiles/BasicTorch.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using SandboxMod.Common;
+using SandboxMod.Content.Dusts;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace SandboxMod.Content.Tiles
+{
+    public class BasicTorch : ModTile
+    {
+        public override bool Autoload(ref string name, ref string texture)
+        {
+            texture = AssetDirectory.GetTexture<BasicTorch>();
+            return base.Autoload(ref name, ref texture);
+        }
+
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type]   = true;
+            Main.tileNoAttach[Type]         = true;
+            Main.tileNoFail[Type]           = true; // ?
+            Main.tileWaterDeath[Type]       = true;
+            Main.tileLavaDeath[Type]        = true;
+            Main.tileLighted[Type]          = true;
+
+            TileID.Sets.FramesOnKillWall[Type] = true;
+
+            drop                = ModContent.ItemType<Items.Tiles.BasicTorch>();
+            dustType            = ModContent.DustType<BasicDust>();
+            adjTiles            = new int[] { TileID.Torches };
+            torch               = true;
+            disableSmartCursor  = true;
+
+            AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTorch);
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Basic Torch");
+            AddMapEntry(new Color(255, 255, 255), name);
+
+            // Is this all?
+            TileObjectData.newTile.CopyFrom(TileObjectData.StyleTorch);
+            TileObjectData.addTile(Type);
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num) =>
+            num = Main.rand.Next(1, 3);
+
+        // Modify all other basic tiles.. again
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+        {
+            // Add light depending on frame
+            (r, g, b) = (255f / byte.MaxValue, 255f / byte.MaxValue, 255f / byte.MaxValue);
+        }
+
+        public override void MouseOver(int i, int j)
+        {
+            Player player = Main.LocalPlayer;
+
+            player.noThrow          = 2;
+            player.showItemIcon     = true;
+            player.showItemIcon2    = ModContent.ItemType<Items.Tiles.BasicTorch>();
+        }
+    }
+}
